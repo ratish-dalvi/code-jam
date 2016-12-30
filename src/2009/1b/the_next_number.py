62882c3f7711042c1202a4d032c8c872568f7d61
Ratish <ratish@wise.io>
2016-12-30 09:33:38 -0800

    smaller solution for the_next_number

--- src/2009/1b/the_next_number.py
+++ src/2009/1b/the_next_number.py
@@ -9,19 +9,14 @@ T = int(f.readline().strip())
 for c in range(T):
     num = list(f.readline().strip())
     if sorted(num, reverse=True) == num:
-        num.sort()
-        for i in range(len(num)):
-            if num[i] > '0':
-                break
-        out = "".join([num[i]] + ['0'] + num[:i] + num[i+1:])
-    else:
-        for i in reversed(range(len(num)-1)):
-            if num[i] < num[i+1]:
-                break
-        for j in range(len(num)-1, i, -1):
-            if num[j] > num[i]:
-                break
-        out = "".join(num[:i] + [num[j]] + sorted(num[i:j] + num[j+1:]))
+        num.insert(0, '0')
+    for i in reversed(range(len(num)-1)):
+        if num[i] < num[i+1]:
+            break
+    for j in range(len(num)-1, i, -1):
+        if num[j] > num[i]:
+            break
+    out = "".join(num[:i] + [num[j]] + sorted(num[i:j] + num[j+1:]))
     print("Case #%d: %s" % (c+1, str(out)))
 
 f.close()
