a74bdf14ed0fa66b64201f46c0e39d8516433cb6
Ratish <ratish@wise.io>
2017-01-03 22:50:41 -0800

    minor changes in recursive solution

--- src/2010/1a/make_it_smooth_1_recursive.py
+++ src/2010/1a/make_it_smooth_1_recursive.py
@@ -1,22 +1,19 @@
 """
 https://code.google.com/codejam/contest/544101/dashboard#s=p1&a=1
 
-Type - Dynamic Programming
+Type - Dynamic Programming using recursion and memoization
 
 Small - 21 seconds
 large - 16 minutes :(
 
 Complexity - O(256 * 256 * n)
 """
-import os
 import sys
 import numpy as np
-sys.path.append(os.path.abspath('./src/helpers'))
 
 f = open(sys.argv[1])
 T = int(f.readline().strip())
 
-inf = 9999999  # can't use np.inf since I want to multiply with 0
 for c in range(T):
     D, I, M, N = map(int, f.readline().strip().split(" "))
     cache = np.ones((N, 256)) * -1
@@ -31,11 +28,10 @@ for c in range(T):
         best = go(pixels, n-1, y) + D
         for x in range(256):  # previous pixel value
             o = go(pixels, n-1, x)
-            best = min(
-                best,
-                o + abs(y - pixels[n]) +
-                (((max(0, abs(y - x) - 1) // max(M, 1)) * I)
-                 if M > 0 else inf*(abs(y-x))))
+            best = min(best,
+                       o + abs(y - pixels[n]) +
+                       (((max(0, abs(y - x) - 1) // max(M, 1)) * I)
+                        if M > 0 else abs(y-x)))
         cache[n][y] = best
         return best
 
