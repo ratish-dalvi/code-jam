bb4d774c95f078f66046fe6a8c8fe82a83d9133e
Ratish <ratish@wise.io>
2017-01-05 14:44:07 -0800

    removed extra line

--- src/2010/qualification/snapper_chain.py
+++ src/2010/qualification/snapper_chain.py
@@ -9,5 +9,4 @@ T = int(f.readline().strip())
 for c in range(T):
     N, K = map(int, f.readline().strip().split(" "))
     print("Case #%d: %s" % (c+1, "OFF" if (K + 1) % (2**N) else "ON"))
-
 f.close()
