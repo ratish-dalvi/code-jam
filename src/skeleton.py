aa95c17bd66baaafc6d81027c5fb564a527ccd14
Ratish <ratish@wise.io>
2017-02-13 23:46:42 -0800

    removed alg call

--- src/skeleton.py
+++ src/skeleton.py
@@ -8,9 +8,6 @@ sys.path.append(os.path.abspath('./src/helpers'))
 f = open(sys.argv[1])
 
 
-def alg():
-    return
-
 T = int(f.readline().strip())
 for c in range(T):
     n = int(f.readline().strip())
@@ -19,7 +16,7 @@ for c in range(T):
     # v1 = map(int, f.readline().strip().split(" "))
     # v2 = map(int, f.readline().strip().split(" "))
 
-    out = alg()
+    # Write algorith here
     print("Case #%d: %s" % (c+1, str(out)))
 
 f.close()
