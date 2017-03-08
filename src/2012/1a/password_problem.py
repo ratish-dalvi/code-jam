4473a96a1dc37d47008ec231cb829e177b548fa0
Ratish <ratish@wise.io>
2017-03-08 11:28:56 -0800

    solution to password_problem

---
+++ src/2012/1a/password_problem.py
@@ -0,0 +1,30 @@
+"""
+Skeleton to solve problems
+"""
+import os
+import sys
+import numpy as np
+sys.path.append(os.path.abspath('./src/helpers'))
+
+f = open(sys.argv[1])
+
+T = int(f.readline().strip())
+for c in range(T):
+    A, B = map(int, f.readline().strip().split(" "))
+    probs = map(float, f.readline().strip().split(" "))
+    cum_probs = [0]*(A + 1)
+    p = 1
+    for i, x in enumerate(probs):
+        p = p * x
+        cum_probs[i+1] = p
+
+    min_ee = np.inf
+    for i in range(A):
+        ee = (cum_probs[A-i]) * (B - A + 2*i + 1) + \
+             (1 - cum_probs[A-i]) * (B - A + 2*i + 1 + B + 1)
+        min_ee = min(min_ee, ee)
+    min_ee = min(min_ee, B + 2)
+
+    print("Case #%d: %.6f" % (c+1, min_ee))
+
+f.close()
