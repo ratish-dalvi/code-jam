152fc269eee66fe14cf0a67e6737b56d2ea7474d
Ratish <ratish@wise.io>
2017-04-10 19:27:05 -0700

    function for getting child path in a binary tree

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,6 +1,7 @@
 # Basic utility functions
 
 from numpy import unravel_index
+import math
 
 
 def np_argmin(x):
@@ -17,3 +18,15 @@ def maximum_contiguous_subarray(lst):
         current_sprint = max(current_sprint + x, 0)
         max_so_far = max(current_sprint, max_so_far)
     return max_so_far
+
+
+def get_path_from_root(x):
+    """ given x, get its path on a binary tree"""
+    l = math.floor(math.log(x, 2))  # level, starting from 0
+    y = x - (2**l - 1)  # number of elements at level l
+    lst = []
+    while l > 0:
+        lst.append("L" if y % 2 else "R")
+        y = math.ceil(y/2)
+        l -= 1
+    return list(reversed(lst))
